7442cd3b4d96bee2a51de5bc9193e3d40669544e
Anatolii Balaban <anatoliibalaban@Anatoliis-MacBook-Pro.local>
2020-02-22 13:36:30 +0200

    commit

---
+++ HomeTasks/first/HomeWork1.java
@@ -0,0 +1,10 @@
+package first;
+
+public class HomeWork1 {
+
+		
+		public static void main (String[] args) {
+			System.out.println ("hello git");
+		}
+	}
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author anatoliibalaban
+ *
+ */
+module sOURCEit {
+}
